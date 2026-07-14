162544ab56ce3e97939c4f1f8a5eda97b6619413
Mujeebhussain153 <mujeebhussain153@gmail.com>
2026-07-14 09:49:34 +0530

    Added a lifespan context manager

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,25 @@
 from fastapi import FastAPI
 from app.core.config import settings
+from contextlib import asynccontextmanager
+from app.db.base import Base
+from app.db.database import engine
+import app.models
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    """
+    Runs once when the application starts.
+    """
+    Base.metadata.create_all(bind=engine)
+
+    yield
+
+    print("Shutting Down DOCRAG-AI")
 
 app = FastAPI(
     title=settings.APP_NAME,
-    version=settings.APP_VERSION
+    version=settings.APP_VERSION,
+    lifespan=lifespan
 )
 
 @app.get("/")
